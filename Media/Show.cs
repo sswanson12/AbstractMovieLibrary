cc0875452e43e9a0c4395c519ad3af91aba620bd
sswanson12 <sswanson12@my.wctc.edu>
2022-03-03 19:47:38 -0600

    Configured show and video classes

--- Media/Show.cs
+++ Media/Show.cs
@@ -2,4 +2,47 @@
 
 public class Show : Media
 {
+    private int _id;
+    private string _title;
+    private int _season;
+    private int _episode;
+    private string[] _writers;
+
+    public Show(string title, int season, int episode, string[] writers)
+    {
+        _title = title;
+        _season = season;
+        _episode = episode;
+        _writers = writers;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Title
+    {
+        get => _title;
+        set => _title = value;
+    }
+
+    public int Season
+    {
+        get => _season;
+        set => _season = value;
+    }
+
+    public int Episode
+    {
+        get => _episode;
+        set => _episode = value;
+    }
+
+    public string[] Writers
+    {
+        get => _writers;
+        set => _writers = value;
+    }
 }
--- Media/Video.cs
+++ Media/Video.cs
@@ -1,5 +1,50 @@
-﻿namespace ApplicationTemplate.Media;
+﻿using System.Linq;
+
+namespace ApplicationTemplate.Media;
 
 public class Video : Media
 {
+    private int _id;
+    private string _title;
+    private string _format;
+    private int _length;
+    private int[] _regions;
+
+    public Video(string title, string format, int length, int[] regions)
+    {
+        _title = title;
+        _format = format;
+        _length = length;
+        _regions = regions;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Title
+    {
+        get => _title;
+        set => _title = value;
+    }
+
+    public string Format
+    {
+        get => _format;
+        set => _format = value;
+    }
+
+    public int Length
+    {
+        get => _length;
+        set => _length = value;
+    }
+
+    public int[] Regions
+    {
+        get => _regions;
+        set => _regions = value;
+    }
 }
